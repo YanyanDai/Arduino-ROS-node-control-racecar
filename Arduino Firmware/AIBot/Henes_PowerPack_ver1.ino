1ac9be896c394d6619dd6d0f43e433a771421593
YanyanDai <47199662+YanyanDai@users.noreply.github.com>
2022-12-16 20:36:56 +0900

    Update Henes_PowerPack_ver1.ino

--- Arduino Firmware/AIBot/Henes_PowerPack_ver1.ino
+++ Arduino Firmware/AIBot/Henes_PowerPack_ver1.ino
@@ -20,6 +20,60 @@ std_msgs::Int32 encoder_data2; //모터 엔코터2 값 전달을 위한 변수 (
 int velocity=0;
 int steer_angle = 0;
 
+void front_motor_control(int motor1_pwm)
+{
+   if (motor1_pwm > 0) // forward
+  {
+    digitalWrite(MOTOR1_ENA, HIGH);
+    digitalWrite(MOTOR1_ENB, LOW);
+    analogWrite(MOTOR1_PWM, motor1_pwm);
+
+  }
+  else if (motor1_pwm < 0) // backward
+  {
+
+    digitalWrite(MOTOR1_ENA, LOW);
+    digitalWrite(MOTOR1_ENB, HIGH);
+    analogWrite(MOTOR1_PWM, -motor1_pwm);
+  }
+  else
+  {
+    digitalWrite(MOTOR1_ENA, LOW);
+    digitalWrite(MOTOR1_ENB, LOW);
+    digitalWrite(MOTOR1_PWM, 0);
+  }
+}
+
+void rear_motor_control(int motor2_pwm)
+{
+   if (motor2_pwm > 0) // forward
+  {
+    digitalWrite(MOTOR2_ENA, HIGH);
+    digitalWrite(MOTOR2_ENB, LOW);
+    analogWrite(MOTOR2_PWM, motor2_pwm);
+
+  }
+  else if (motor2_pwm < 0) // backward
+  {
+
+    digitalWrite(MOTOR2_ENA, LOW);
+    digitalWrite(MOTOR2_ENB, HIGH);
+    analogWrite(MOTOR2_PWM, -motor2_pwm);
+  }
+  else
+  {
+    digitalWrite(MOTOR2_ENA, LOW);
+    digitalWrite(MOTOR2_ENB, LOW);
+    digitalWrite(MOTOR2_PWM, 0);
+  }
+}
+
+void motor_control(int front_speed, int rear_speed)
+{
+ front_motor_control(front_speed);
+ rear_motor_control(rear_speed);  
+}
+
 void cmd_vel_callback(const geometry_msgs::Twist&  msg){
   velocity = (int)msg.linear.x; //속도 제어
   steer_angle = (int)msg.angular.z; //steering motor 각도 제어
